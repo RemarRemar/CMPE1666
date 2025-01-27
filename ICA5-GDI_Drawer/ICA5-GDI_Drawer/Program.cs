3b352bb1cce7a7175c84500021c812cb7678e42f
RemarRemar <remar.gabriel.b@gmail.com>
2025-01-27 13:34:40 -0700

    updoot

---
+++ ICA5-GDI_Drawer/ICA5-GDI_Drawer/Program.cs
@@ -0,0 +1,27 @@
+﻿/*Remar Gabriel Espaldon Bacadon  
+ *GDI Drawer
+ * 
+ */
+using System;
+using System.Drawing;//needed for GDI Drawer
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GDIDrawer;
+
+namespace ICA5_GDI_Drawer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //variables
+            int iDiameter;
+            int iX;
+            int iY;
+            //
+            CDrawer Canvas = new CDrawer(300, 300);
+        }
+    }
+}
