9afa5761febacafe35416b9c09eb6ddb9758ee57
RemarRemar <remar.gabriel.b@gmail.com>
2025-04-03 16:26:59 -0600

    updoot

--- Lab 3 - Methods/Lab 3 - Methods/Program.cs
+++ Lab 3 - Methods/Lab 3 - Methods/Program.cs
@@ -3,19 +3,100 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
+using GDIDrawer;
 namespace Lab_3___Methods
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            double dX = 0, dA, dB, dC, dXLow, dXHigh, dFunctionOfX;
-            Title("Lab 3 methods");
-            GetCoefficients(out dA, out dB, out dC);
-            GetRange(out dXLow, out dXHigh);
-            Quadratic(dA, dB, dC, dX, out dFunctionOfX);
+            double dX = 0, dA, dB, dC, dYLow, dYHigh, dY;
 
+            CDrawer Canvas = new CDrawer(960, 1080);
+
+            bool bKeepGoing = true;
+
+            do
+            {
+                Title("Lab 3 methods");
+                GetCoefficients(out dA, out dB, out dC);
+                GetRange(out dYLow, out dYHigh);
+                DrawGraph(dA, dB, dC, dYLow, dYHigh, Canvas); 
+                
+
+            } while (YesNo("\nRun again? yes or no: "));
             Console.ReadKey();
+        }
+        static private void DrawGraph(double dA, double dB, double dC, double dYLow, double dYHigh, CDrawer Canvas)
+        {
+            double dY, dX = 0;
+
+            if (dYLow >= 0)
+            {
+                dYLow = 400 + (dYLow * 50);
+            }
+            else
+            {
+                dYLow = 400 - (dYLow * 50);
+            }
+
+            bool bDrawNo = false, bDrawRight = true, bDrawleft = true;
+            do
+            {
+                double dY1 = 0, dY2 = 0, dX1 = 0, dX2 = 0;
+
+                if (bDrawRight)
+                {
+                    Quadratic(dA, dB, dC, dX, out dY);
+
+                    
+                    if (dA > 0)
+                    {
+                        dY = 400 - dY;
+                    }
+                    else
+                    {
+                        dY += 400;
+                    }
+
+                    Quadratic(dA, dB, dC, dX1, out dY1);
+
+                    if (dY >= dYLow && dY <= dYHigh)
+                    {
+
+                    }
+                }
+
+
+            } while (!bDrawNo);
+
+        }
+        static private bool YesNo(in string sMessage)
+        {
+            bool bYesnt = true, bOut = true;
+            do
+            {
+                Console.WriteLine(sMessage);
+                string sYesnt = Console.ReadLine();
+                sYesnt = sYesnt.ToLower();
+                switch (sYesnt)
+                {
+                    case "yes":
+                        bYesnt = true;
+                        bOut = true;
+                        break;
+                    case "no":
+                        bYesnt = false;
+                        bOut = true;
+                        break;
+                    default:
+                        bOut = false;
+                        break;
+                }
+            } while (!bOut);
+
+            return (bYesnt);
         }
         static private void GetCoefficients(out double dA, out double dB, out double dC)
         {
@@ -26,14 +107,15 @@ namespace Lab_3___Methods
             GetValue(out dC, "Enter a value for c: ");
             
         }
-        static private void GetRange(out double dXLow, out double dXHigh)
+        static private void GetRange(out double dYLow, out double dYHigh)
         {
-            GetValue(out dXLow, " Enter the lower limit: ");
-            GetValue(out dXHigh, " Enter the higher limit: ", dXLow);
+            GetValue(out dYLow, " Enter the lower limit: ");
+            GetValue(out dYHigh, " Enter the higher limit: ", dYLow);
         }
         static private void Quadratic(double dA, double dB, double dC, double dX, out double dFofX)
         {
-
+            dFofX = (dA * Math.Pow(dX, 2)) + (dB * dX) + dC;
+            
         }
         static private void GetValue(out double dOut, string sIn)
         {
