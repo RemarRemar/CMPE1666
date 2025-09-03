046e274ea162ce17928cadf5c4aa1c5884ceedea
RemarRemar <remar.gabriel.b@gmail.com>
2025-09-03 09:53:34 -0600

    updoot!

---
+++ DemoOrderOfN/DemoOrderOfN/Program.cs
@@ -0,0 +1,71 @@
+﻿//***********************************************************************************
+//Program: OrderOfN.cs
+//Description: Calculates sum of given numbers
+//Date: Today
+//Author: Remar Bacadon
+//Course: CMPE1300
+//Class: CNTA01
+//***********************************************************************************
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoOrderOfN
+{
+    internal class Program
+    {
+         static void Main(string[] args)
+        {
+            int[] ArrayOfN;
+
+            Console.WriteLine("How many Numbers would you like to generate?");
+            int.TryParse(Console.ReadLine(), out int NumInArray);
+            ArrayOfN = new int[NumInArray];
+
+            PopulateArray(ArrayOfN);
+            GetSum(ArrayOfN);
+            Console.ReadKey();
+        }
+        static void PopulateArray(int[] ArrayOfNumbers)
+        {
+            //********************************************************************************************
+            //Method: PopulateArray(int[] Array)
+            //Purpose: generates random numbers to populate an array
+            //Parameters:Random number generator
+            // Assign numbers to Array
+            //Returns: populated Array
+            //*********************************************************************************************
+
+            Random random = new Random();
+
+            for (int i = 0; i <= ArrayOfNumbers.Length; i++)
+            {
+                ArrayOfNumbers[i] = random.Next(1, 9);
+                Console.Write($"{ArrayOfNumbers[i]}, ");
+            }
+            return;
+        }
+        static int GetSum(int[] ArrayOfNumbers)
+        {
+            //********************************************************************************************
+            //Method: GetSum(int[] ArrayOfNumbers)
+            //Purpose: adds up all the numbers in the array
+            //Parameters:number adder loop
+            // print the sum
+            //Returns: the sum
+            //*********************************************************************************************
+
+            int sum = 0;
+            for (int i = 0; i < ArrayOfNumbers.Length; i++)
+            {
+                sum += ArrayOfNumbers[i];
+            }
+
+            Console.WriteLine($"The sum of all the generated numbers is {sum}");
+            return (sum);
+        }
+    }
+}
--- DemoOrderOfN/DemoOrderOfN/Properties/AssemblyInfo.cs
+++ DemoOrderOfN/DemoOrderOfN/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DemoEnums")]
+[assembly: AssemblyTitle("DemoOrderOfN")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("NAIT")]
-[assembly: AssemblyProduct("DemoEnums")]
-[assembly: AssemblyCopyright("Copyright © NAIT 2024")]
+[assembly: AssemblyProduct("DemoOrderOfN")]
+[assembly: AssemblyCopyright("Copyright © NAIT 2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d9559c60-71fb-4f0c-a248-98e477cc49fd")]
+[assembly: Guid("588a9cda-3da0-4fbd-af96-6781833da8e6")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,8 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
