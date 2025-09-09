82ec77b97f58295b9d7e66884dba4641485d6f7a
RemarRemar <remar.gabriel.b@gmail.com>
2025-09-08 22:11:43 -0600

    Finished ICA01
    add user prompts for repeated searches, added array generation and display. completed the program as required basically.

--- ICA01-Linear_Search/ICA01-Linear_Search/Program.cs
+++ ICA01-Linear_Search/ICA01-Linear_Search/Program.cs
@@ -20,9 +20,37 @@ namespace ICA01_Linear_Search
         static int iMin = 1, iMax = 255;// the upper and lower limit for every getvalue
         static void Main(string[] args)
         {
+            bool bLoop = true;//if true keep looping
             int ilengthOfArray = GetValue("give a number for the length of array", iMin, iMax);// ask value for array size
             GetRange(out int iLowRange, out int iUpRange);
-            Console.WriteLine($"LArray={ilengthOfArray}, DR={iLowRange}, UR={iUpRange}");
+            int[] iArray = GenerateArray(ilengthOfArray, iLowRange, iUpRange);
+            DisplayArray(iArray);
+            while (bLoop)// to make them loop if they want to search for diffrent value occurence
+            {
+                bool bAsking = true;
+                CountOccurences(iArray, GetValue("\nEnter the value to be searched", iMin, iMax));
+                while (bAsking)//to make them loop if they dont answer y or n
+                {
+                    Console.Write("Do you want to search for another value (y/n)? ");//asking the q
+                    string yesnt = Console.ReadLine();
+                    yesnt = yesnt.ToLower();
+
+                    switch (yesnt)//feeling fancy
+                    {
+                        case "n"://get out of the search value loop thus ending the run
+                            Console.WriteLine("bye bye");
+                            bLoop = false;
+                            bAsking = false;
+                            break;
+
+                        case "y"://get out of the search again question loop
+                            bAsking = false;
+                            break;
+                        default://loop again with the question asking if they want to search again
+                            break;
+                    }
+                }
+            }
         }
 
         //********************************************************************************************
@@ -42,7 +70,7 @@ namespace ICA01_Linear_Search
             bool bNumber = false;//boolean to validate if the user gave an integer
             while (bLoop) //loop if yes
             {
-                Console.Write($"{strPrompt}, betweeen {iMinV} and {iMaxV}: ");//tell them what numbers they can put
+                Console.Write($"\n{strPrompt}, betweeen {iMinV} and {iMaxV}: ");//tell them what numbers they can put
                 bNumber = int.TryParse(Console.ReadLine(), out iValue); // they must give an integer
                 if (bNumber && iValue >= iMinV && iValue <= iMaxV)//input is integer and within the range
                 {
@@ -50,7 +78,7 @@ namespace ICA01_Linear_Search
                 }
                 else//input not valid
                 {
-                    Console.WriteLine($"\nEntered an invalid number\n");//tell them its not valid
+                    Console.WriteLine($"Entered an invalid number");//tell them its not valid
                 }
             }
 
@@ -73,17 +101,17 @@ namespace ICA01_Linear_Search
             bool bloop = true;
             while (bloop)
             {
-                iMinR = GetValue("\ngive the minimum for the range of random generated numbers", iMin, iMax);
-                iMaxR = GetValue("\ngive the maximum for the range of random generated numbers", iMin, iMax);
+                iMinR = GetValue("give the minimum for the range of random generated numbers", iMin, iMax);
+                iMaxR = GetValue("give the maximum for the range of random generated numbers", iMin, iMax);
 
                 while(iMinR == iMax)// lower range given shouldn't be at the max value, because they wont be able to give a valid upper rnge
                 {
-                    Console.WriteLine($"You cant have the minimum be at {iMax}");
+                    Console.WriteLine($"\nYou cant have the minimum be at {iMax}");
                     iMinR = GetValue("give the minimum for the range of random generated numbers", iMin, iMax);
                 }
                 while (iMaxR <= iMinR)// maximum given shouldnt be larger or equal to the minimum given, have them fix it!
                 {
-                    Console.WriteLine($"You cant have the maximum be the same as minimum, it must be larger than the minimum({iMinR}) you input");
+                    Console.WriteLine($"\nMaximum must be larger than the minimum({iMinR})");
                     iMaxR = GetValue("give the maximum for the range of random generated numbers", iMin, iMax);
                 }
                 if(iMinR != iMax && iMaxR > iMinR)//check if everything is good before leaving, that the upper range is bigger than the lower range.
@@ -94,40 +122,67 @@ namespace ICA01_Linear_Search
         }
 
         //********************************************************************************************
-        //Method: GetSum(int[] ArrayOfNumbers)
-        //Purpose: adds up all the numbers in the array
-        //Parameters:number adder loop
-        // print the sum
-        //Returns: the sum
+        //Method: GenerateArray(int iArraySize, int iMinA, int iMaxA)
+        //Purpose: creates an array
+        //Parameters:value for array size, lower range, upper range
+        //Generate the array
+        //Returns: Generated array
         //*********************************************************************************************
-        static void GenerateArray()
+        static int[] GenerateArray(int iArraySize, int iMinA, int iMaxA)
         {
+            int[] iNewArray = new int[iArraySize];//create the array
+            Random rand = new Random();//random generator
+            for(int i = 0; i < iArraySize; i++)//loop for the size of the array
+            {
+                iNewArray[i] = rand.Next(iMinA, iMaxA + 1);//put a random number within the range given by user in each index of the array
+            }//+1 cause rand.Next max is -1 of given
 
-
+            return (iNewArray);
         }
 
         //********************************************************************************************
-        //Method: GetSum(int[] ArrayOfNumbers)
-        //Purpose: adds up all the numbers in the array
-        //Parameters:number adder loop
-        // print the sum
-        //Returns: the sum
+        //Method: DisplayArray(int[] iArrayToDisplay)
+        //Purpose: displays the array/print
+        //Parameters:the array
+        //display array in an inefficient way
+        //Returns: nothing
         //*********************************************************************************************
-        static void DisplayArray()
+        static void DisplayArray(int[] iArrayToDisplay)
         {
+            Console.Write("\nthe generated values are: ");
+            for (int i = 0; i < iArrayToDisplay.Length; i++)//prints/displays the array
+            {
+                Console.Write($"{iArrayToDisplay[i]}, ");
 
 
+            }
         }
 
         //********************************************************************************************
-        //Method: GetSum(int[] ArrayOfNumbers)
-        //Purpose: adds up all the numbers in the array
-        //Parameters:number adder loop
-        // print the sum
-        //Returns: the sum
+        //Method: CountOccurences(int[] iArrayToSift, int iOccurence)
+        //Purpose: find a specific value in the array
+        //Parameters:array
+        //loop and compare values to find the Occurence
+        //Returns: the Occurences
         //*********************************************************************************************
-        static void CountOccurences()
+        static void CountOccurences(int[] iArrayToSift, int iOccurence)
         {
+            int tempj = 0;//for counting
+            for (int i = 0; i < iArrayToSift.Length; i++)
+            {
+                if (iOccurence == iArrayToSift[i])//counts up if a value matching required is found
+                {
+                    tempj++;
+                }
+            }
+            if (tempj > 0)
+            {
+                Console.WriteLine($"{iOccurence} occurs {tempj} times in the array.");
+            }
+            else
+            {
+                Console.WriteLine($"{iOccurence} cannot be found");
+            }
 
 
         }
