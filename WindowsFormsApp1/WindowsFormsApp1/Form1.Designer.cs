02a2f36c5be77bb41313eed2af7b904830b9fadc
RemarRemar <remar.gabriel.b@gmail.com>
2025-09-10 09:50:08 -0600

    Add initial Windows Forms application
    Created a new C# Windows Forms project with a main form (Form1) that handles mouse enter, leave, click, and double-click events. Includes solution, project, designer, resource, and configuration files necessary to build and run the application.

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(707, 470);
+            this.Name = "Form1";
+            this.Text = "something";
+            this.DoubleClick += new System.EventHandler(this.Form1_DoubleClick);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.MouseEnter += new System.EventHandler(this.Form1_MouseEnter);
+            this.MouseLeave += new System.EventHandler(this.Form1_MouseLeave);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        int iClickCounter = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Form1_MouseEnter(object sender, EventArgs e)
+        {
+            Text = "Mouse is in :)";
+            BackColor = Color.Green;
+        }
+
+        private void Form1_MouseLeave(object sender, EventArgs e)
+        {
+            Text = "Mouse Left :(";
+            iClickCounter = 0;
+            BackColor = Color.Red;
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            //Text = $"MOUSE CLICKED :{++iClickCounter}";
+           //Random rand = new Random();
+            //BackColor = Color.FromArgb(rand.Next(50, 200), rand.Next(50, 200), rand.Next(50, 200));
+        }
+
+        private void Form1_DoubleClick(object sender, EventArgs e)
+        {
+            if((++iClickCounter % 2) == 0)
+            {
+                Text = "Nice Form";
+            }
+            else
+            {
+                Text = $"MOUSE CLICKED :{iClickCounter}";
+            }
+        }
+    }
+}
